77194ca75236b626edaadc247ad75e119ec0149d
Ben Pearl <ben.pearl@btinternet.com>
2024-07-26 13:06:38 +0100

    updated routing

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // Replace `YOUR_API_URL` with the actual URL of your Flask backend endpoint
-const YOUR_API_URL = 'https://user-auth-backend-cown25ubvq-nw.a.run.app'; // Adjust to fit your actual Flask endpoint
+const YOUR_API_URL = 'https://user-auth-backend-cown25ubvq-nw.a.run.app/api/test'; // Adjust to fit your actual Flask endpoint
 const getTestData = async () => {
   try {
     const response = await axios.get(YOUR_API_URL);
